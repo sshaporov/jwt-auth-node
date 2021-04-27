f36172cbe54fa0ea3774fb6ccb8b70b82662c613
Sergey Shaporov <sshaporov86@gmail.com>
2021-04-27 17:37:26 +0300

    fix mongoose warning to use FindAndModify fn

--- helpers/init-mongodb.js
+++ helpers/init-mongodb.js
@@ -4,6 +4,7 @@ mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false,
 })
     .then(() => {
         console.log('MongoDB connected')
